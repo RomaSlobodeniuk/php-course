1fb75b391da640b9ccc01aa8304afa109e3d5b4f
Andrey <a@site.ru>
2018-10-13 15:01:37 +0300

    add code to DZ lesson 6

--- NeoMaximus666/6_lesson/dz.php
+++ NeoMaximus666/6_lesson/dz.php
@@ -17,14 +17,45 @@ function myStrArray($string, $listWord)
 {
     $newArray = array();
 //    $countList = count($listWord);
+    function myPosChartFunction($string, $word,&$pos) {
+        $pos = strpos($string, $word, $pos);
+
+        return $pos;
+    }
     foreach ($listWord as $i => $word)
     {
         $countStr = substr_count($string,$word);
         $newArray[$word] = $countStr;
-    }
-    return $newArray;
-}
 
-var_dump(myStrArray($content,$strArray));
+
+
+        echo "<hr>";
+        //strpos — Возвращает позицию первого вхождения подстроки
+        // Заметьте, что используется ===.  Использование == не даст верного
+        // результата, так как 'a' находится в нулевой позиции.
+        $pos = 0;
+        for($posChart = 0;$posChart<$countStr;$posChart++){
+
+
+
+            $thisPos = myPosChartFunction($string, $word,$pos);
+            $pos = $pos + 1;
+
+            if ($thisPos === false) {
+                echo "Строка '$word' не найдена в строке '$string'";
+            } else {
+                echo "Строка '$word' найдена в тексте";
+                echo " на позиции $thisPos";
+            }
+            echo "<br>";
+        }
+    }
+
+    var_dump($newArray);
+//    return $newArray;
+}
+myStrArray($content,$strArray);
+//var_dump(myStrArray($content,$strArray));
+
 
 ?>
