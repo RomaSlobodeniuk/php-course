975825c185b7025fe8c35c05910b4b8907c9930b
ShtelNik <shtelnick@gmail.com>
2018-11-01 11:29:35 +0200

    no comment

---
+++ ShtelNik/HTML_Les12/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- Comment -->
+    <link rel="stylesheet" href="../assets/styles.css">
+    <script rel="script" src="../assets/script.js"></script>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body class="background-image">
