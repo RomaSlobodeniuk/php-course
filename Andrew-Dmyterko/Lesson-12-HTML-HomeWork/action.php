4833c53542901857e5ffe853d5c3c89edba3311f
Andrew-Dmyterko <andrew.dmyterko@gmail.com>
2018-11-05 22:38:27 +0200

    Очередное обновление. добавил функционал к сайту (добавление новой картинки, изменение контента сайта). p.s. свои парсера к текстовым файлам

--- Andrew-Dmyterko/Lesson-12-HTML-HomeWork/action.php
+++ Andrew-Dmyterko/Lesson-12-HTML-HomeWork/action.php
@@ -10,27 +10,37 @@
 require_once '../helpers/functions.php';
 require_once './add_bootstrap.php';
 
+
 $params = $_POST;
-
-$users = [
-  1=>  [       'user'     => 'andrew.dmyterko@gmail.com',
-            'password' => '1234',
-            'name'     => 'Andrew Dmyterko'],
-
-  2=>  [       'user'     => 'sky_fox123@ukr.net',
-            'password' => '701051',
-            'name'     => 'Ivanov Ivan'],
-];
+//if (!empty($params['email'])) {
+//
+//    print_r($params);
+//    print_r($_FILES);
+//}
+// База данных пользователей
+require_once './pars_file_users_to_array.php';
+//$users = [
+//  1=>  [    'user'     => 'andrew.dmyterko@gmail.com',
+//            'password' => '1234',
+//            'name'     => 'Andrew Dmyterko'
+//  ],
+//
+//  2=>  [    'user'     => 'sky_fox123@ukr.net',
+//            'password' => '701051',
+//            'name'     => 'Ivanov Ivan'
+//  ],
+//];
 
 //var_dump($users);
 
 function pass($user_name, $passwd, $basa) {
-    $good = false;
+    $good = -1;
 
    foreach ($basa as $kk => $key) {
         if ($user_name===$key['user'] && $passwd===$key['password']) {
 //            echo $key['password'], $key['user'],$kk;
             $good = "$kk";
+
         }
     }
     return $good;
@@ -54,7 +64,7 @@ function pass($user_name, $passwd, $basa) {
                     ВВедите E-Mail!!!!
                 </div>
     <?php endif; ?>
-    <?php if (pass($params['email'], $params['passwd'], $users)): ?>
+    <?php if ((pass($params['email'], $params['passwd'], $users))>=0 && $error==0): ?>
                 <!-- если пароль правильный Выводим заставку Secret Area-->
 
                 <div class="jumbotron jumbotron-fluid">
@@ -63,19 +73,36 @@ function pass($user_name, $passwd, $basa) {
                         <p class="lead"><em> Your password is -- <?php echo $params['passwd']; ?> </em></p>
                         <p class="lead"><em> Your MD5 password hash  -- <?php echo md5($params['passwd']); ?> </em></p>
                         <p class="lead"><em> Your E-Mail is  -- <?php echo $params['email']; ?> </em></p>
-                        <hr class="my-4">
+                        <div class="row">
+                            <a class="btn btn-primary" href="./index.php#login" role="button">Вернуться назад</a>
+
+                            <form name="form1" enctype="multipart/form-data" action="./admin.php" method="post">
+<!--                        <a class="btn btn-danger" href="./admin.php" role="button">Добавить картинку</a>-->
+                                <input type="hidden" name="add_pic" value="add_pic">
+                                <input type="submit" class="btn btn-danger" value="Добавить картинку">
+                            </form>
+
+                            <form name="form1" enctype="multipart/form-data" action="./admin.php" method="post">
+                                <input type="hidden" name="change" value="change">
+                                <input type="submit" class="btn btn-primary" value="Изменить данные">
+                            </form>
+<!--                        <a class="btn btn-danger" href="./admin1.php" name="change" role="button">Изменить данные</a>-->
+                        </div>
+                        <hr class="my-8">
                         <p><b>Note:</b> This is a secret data do not pass them on to third persons.</p>
                     </div>
                 </div>
-    <?php else: $error++;  ?>
+
+
+    <?php elseif($error==0): $error++;  ?>
                 <div class="alert alert-primary" role="alert">
                     Неправильный логин пароль!!!
                 </div>
     <?php endif; ?>
 
     <?php if ($error): ?>
-                <form class="col-sm-6" id="main-form" name="form" method="post" enctype="multipart/form-data" action="./index.php">
-                    <button type="Вернуться назад" class="btn btn-primary">Submit</button>
+                <form class="col-sm-6" id="main-form" name="form" method="post" enctype="multipart/form-data" action="./index.php#login">
+                    <button type="submit" class="btn btn-primary">Вернуться назад</button>
                 </form>
     <?php endif; ?>
 
@@ -87,11 +114,5 @@ function pass($user_name, $passwd, $basa) {
 <!--//    print_r($_FILES);-->
 <!--////    die();-->
 <!--//}-->
-
-
-
-
-
-
 
 <?php require_once './footer.php'; ?>
--- Andrew-Dmyterko/Lesson-12-HTML-HomeWork/main.php
+++ Andrew-Dmyterko/Lesson-12-HTML-HomeWork/main.php
@@ -4,7 +4,6 @@
         <div class="container">
         <h1 class="display-4"><u> World War II Weapons</u></h1>
             <p class="lead"><em>World War II (often abbreviated to WWII or WW2), also known as the Second World War, was a global war that lasted from 1939 to 1945.</em></p>
-        <hr class="my-4">
         <p>I<em> World war is generally said to have begun on 1 September 1939, with the invasion of Poland by Germany...</em></p>
         <a class="btn btn-primary btn-lg" href="https://en.wikipedia.org/wiki/World_War_II" target="_blank" role="button" >To learn more...</a>
     </div>
@@ -16,6 +15,7 @@
         <div class="container">
             <h1 class="display-4"><u>List of World War II weapons</u></h1>
             <p class="lead"><em>World War II saw rapid technological innovation in response to the needs of the various combatants. Many different weapons systems evolved as a result.</em></p>
+            <hr class="my-8">
             <p><b>Note:</b> This list does not consist of all weapons used by all countries in World War II.</p>
         </div>
     </div>
@@ -36,17 +36,8 @@
         <?php endif;?>
     </div>
 
-<!--    <!--Выводим заставку перед логином-->
-<!--<br>-->
-<!--    <div class="jumbotron jumbotron-fluid">-->
-<!--        <div class="container">-->
-<!--<!--            <h1 class="displaперед логиномy-4"><u>List of World War II weapons</u></h1>-->-->
-<!--            <p class="lead"><em>Please enter your login and password to enter the secret area</em></p>-->
-<!--            <p><b>Note:</b> This list does not consist of all weapons used by all countries in World War II.</p>-->
-<!--        </div>-->
-<!--    </div>-->
-
     <div class="jumbotron">
+        <a name="login"></a>
         <p class="lead"><em>Please enter your login and password to enter the secret area</em></p>
         <div class="row justify-left-left">
             <form class="col-sm-6" id="main-form" name="form" method="post" enctype="multipart/form-data" action="./action.php">
@@ -63,14 +54,14 @@
                     <label for="exampleInputPassword2">Confirm Password</label>
                     <input name="passwd_confirm" type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirm Your Password" required>
                 </div>
-                <div class="form-group">
-                    <label for="exampleFormControlFile1">Choose file to load</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
+<!--                <div class="form-group">-->
+<!--                    <label for="exampleFormControlFile1">Choose file to load</label>-->
+<!--                    <input name="file_f" type="file" class="form-control-file" id="exampleFormControlFile1">-->
+<!--                </div>-->
+<!--                <div class="form-group form-check">-->
+<!--                    <input name="check_c" type="checkbox" class="form-check-input" id="exampleCheck1">-->
+<!--                    <label class="form-check-label" for="exampleCheck1">Check me out</label>-->
+<!--                </div>-->
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
