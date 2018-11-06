a13cc48414b1b5b7c27c1883fd25848465a24c74
ShtelNik <shtelnick@gmail.com>
2018-11-06 19:42:14 +0200

    no comment

--- ShtelNik/HTML_Les12/index.php
+++ ShtelNik/HTML_Les12/index.php
@@ -9,6 +9,24 @@ if (!empty($params['email'])) {
     die();
 }
 
+$pictures = [
+  [
+      'name' => 'Black Humor',
+      'src' => '../assets/images/black-humor.jpg',
+      'text' => 'Text 1 goes here'
+  ] ,
+  [
+      'name' => 'Smiled Face',
+      'src' => '../assets/images/smile.jpeg',
+      'text' => 'Text 2 goes here'
+  ] ,
+  [
+      'name' => 'Cute Cat',
+      'src' => '../assets/images/cat.jpg',
+      'text' => 'Text 3 goes here'
+  ]
+];
+
 require_once './header.php';
 require_once './main.php';
 require_once './footer.php';
