51fcde8595996d5107b4f51ce95369e6e25f9ec5
Roma Slobodeniuk <romsl1991@gmail.com>
2018-11-06 20:09:08 +0200

    login form logic

--- RomaSlobodeniuk/10_11_lesson_HTML/login.php
+++ RomaSlobodeniuk/10_11_lesson_HTML/login.php
@@ -3,6 +3,20 @@
 require_once './helpers/functions.php';
 require_once './config/config.php';
 
+$params = $_POST;
+if (isset($params['email']) && empty($params['email'])) {
+    debug("Введіть Ваш EMAIL!");
+    debug($params);
+} elseif (!empty($params['pass1']) && !empty($params['pass2'])) {
+    if ($params['pass1'] !== $params['pass2']) {
+        debug("Паролі не співпадають!");
+        debug($params);
+    } else {
+        debug("Вітаю, ви новий користувач у системі!");
+        debug($params);
+    }
+}
+
 $fileName = './source/login.json';
 $sourceData = getSourceData($fileName);
 
