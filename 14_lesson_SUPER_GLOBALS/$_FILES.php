7644a04db721f26c19ad52c4e4c22cd048557803
Roma Slobodeniuk <romsl1991@gmail.com>
2018-11-08 21:01:31 +0200

    14-th lesson files new

--- 14_lesson_SUPER_GLOBALS/$_FILES.php
+++ 14_lesson_SUPER_GLOBALS/$_FILES.php
@@ -14,7 +14,7 @@ if (!empty($_FILES['name_of_input'])) {
         // basename() може запобігти атаці на файлову систему;
         // може бути доцільним додатково перевірити ім'я файлу;
         $name = basename($_FILES["name_of_input"]["name"]);
-        if (move_uploaded_file($tmp_name, "$uploads_dir/$name")) {
+        if (move_uploaded_file($tmp_name, $uploads_dir . $name)) {
             $_SESSION['file_path'] = $uploads_dir . $name;
         }
     }
@@ -22,7 +22,7 @@ if (!empty($_FILES['name_of_input'])) {
 
 ?>
 
-<?php if (file_exists($_SESSION['file_path'])): ?>
+<?php if (!empty($_SESSION['file_path']) && file_exists($_SESSION['file_path'])): ?>
     <h1>Your last uploaded file:</h1>
     <img src="<?= $_SESSION['file_path'] ?>" width="500px" height="400px">
 <?php endif; ?>
