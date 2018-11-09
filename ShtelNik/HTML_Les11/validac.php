d5b3481c1e2a3d91eeb72f2ccf9980a51719b361
ShtelNik <shtelnick@gmail.com>
2018-11-09 13:08:37 +0200

    no comment

---
+++ ShtelNik/HTML_Les11/validac.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Les11.HTML</title>
+    <style>
+        .red {
+            color: red
+        }
+    </style>
+    <!--coment-->
+    <link rel="stylesheet" href="../assets/style.css">
+    <script async="async" rel="script" src="../assets/script.js"></script>
+<!--    <script defer="defer" rel="script" src="../assets/script.js"></script>  -->
+</head>
+<body class="background-image">
+    <h1 class = "red">Валадація</h1>
+    <p>
+    </p>
+        Вы ввели логин:<br>
+
+        Вы ввели пароль:<br>
+
+        <input type="text" name="login" required value="Vasja">
+        <input type="password" name="password" value="111">
+        <br><br>
+        Вставляй файл!!! <br>
+        <input type="file" name="file"><br>
+        И напиши чето просебя. Прям сюды:<br>
+        <textarea name="coment" cols="50"></textarea><br>
+    <form>
+    </form>
+    <br>
+    <br>
+    <a href = "http://google.com.ua">Link to Google
+        <p> Text Article</p>
+    </a>
+    <br>
+       <span>__Span1__</span>    <span>  Span2  </span>    <span>==Span3==</span>
+    <!--
+        <script>
+            var article = window.document.getElementById('article-1');
+            consjle.log(article.getAttribute('class'))
+        </script>
+        -->
+</body>
+</html>
