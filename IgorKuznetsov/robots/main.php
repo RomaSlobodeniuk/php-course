7f11af817cc65d86cb3043bb982e1409684fedd2
devsize <artrisefrag@gmail.com>
2018-11-13 18:40:44 +0200

    my robots project

---
+++ IgorKuznetsov/robots/main.php
@@ -0,0 +1,32 @@
+<header>
+
+</header>
+<section>
+<div>
+    <article id="article-1" class="article">
+        <h1 class="article-head">Robot`s Name</h1>
+        <img src="img/robo1.jpg" class="robo-img" alt="Robot" title="Robot"><br>
+        <a href="#" target="_blank" class="tec-char-link">Technical characteristics</a><br>
+        <p class="article-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Cumque magnam odit quo? Architecto aspernatur assumenda consectetur consequatur cum eius fuga ipsam
+            laboriosam nisi numquam odit quam quas soluta suscipit, ut. Consequuntur debitis dicta eius fuga iusto.
+            Commodi, deleniti deserunt dolore doloribus ducimus enim ex, excepturi expedita libero magnam nobis officiis
+            praesentium rerum ullam voluptates. Aliquam enim fuga harum nemo officia!
+        </p>
+    </article>
+
+    <form name="robo-form" method="get" enctype="multipart/form-data">
+        <input type="text" name="login" required>
+        <input type="password" name="password" placeholder="Pls enter your password" required>
+        <input type="email" name="email" required><br>
+        <input type="hidden" name="info"><br>
+        <label><input type="checkbox" name="chosen-robot" value="1" checked> Robot 1</label><br>
+        <label><input type="checkbox" name="chosen-robot" value="2"> Robot 2</label><br>
+        <label><input type="checkbox" name="chosen-robot" value="3"> Robot 3</label><br>
+        <label><input type="checkbox" name="chosen-robot" value="4"> Robot 4</label><br>
+        <label><input type="checkbox" name="chosen-robot" value="5"> Robot 5</label><br>
+        <textarea name="comment"></textarea>
+        <input type="submit" value="Send"><br>
+    </form>
+</div>
+</section>
