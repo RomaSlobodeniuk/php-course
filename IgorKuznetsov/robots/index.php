01cebf7f43d5927b420410fe807993d3e4a899b8
devsize <artrisefrag@gmail.com>
2018-11-22 01:09:39 +0200

    robots commit 4

--- IgorKuznetsov/robots/index.php
+++ IgorKuznetsov/robots/index.php
@@ -6,6 +6,40 @@ if (!empty($params)) {
     print_r($params);
 }
 
+$images = [
+    [
+        'name' => 'robotoy retro',
+        'src' => 'img/00387_robotoy_retro_thumb_alpha.png',
+        'description' => 'Some robot 1. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ],
+    [
+        'name' => 'looksee retro',
+        'src' => 'img/00417_looksee_retro_thumb_alpha.png',
+        'description' => 'Some robot 2. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ],
+    [
+        'name' => 'retro daddy',
+        'src' => 'img/00428_retro_daddy_thumb_alpha.png',
+        'description' => 'Some robot 3. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ],
+    [
+        'name' => 'retro pups',
+        'src' => 'img/00451_retro_pups_thumb_alpha.png',
+        'description' => 'Some robot 4. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ],
+    [
+        'name' => 'retro pirate',
+        'src' => 'img/00456_retro_pirate_thumb_alpha.png',
+        'description' => 'Some robot 5. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ],
+    [
+        'name' => 'cheburashka',
+        'src' => 'img/00465_cheburashka_thumb_alpha.png',
+        'description' => 'Some robot 6. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    ]
+];
+
+
 require_once ('header.php');
 require_once ('main.php');
 require_once ('footer.php');
--- IgorKuznetsov/robots/main.php
+++ IgorKuznetsov/robots/main.php
@@ -32,7 +32,7 @@
 </div>
 </div>
 
-<!--main content-->
+<!--top content-->
 
 <section class="main-content">
     <div class="jumbotron jumbotron-fluid">
@@ -51,145 +51,77 @@
         </div>
     </div>
 </section>
+
+<!-- cards-->
+
 <section>
-
-<!--main cards-->
-
     <div id="cards" class="jumbotron jumbotron-fluid">
         <div class="container-fluid row">
-            <div class="card col-lg-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div><div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+            <?php if(!empty($images)):?>
+                <?php foreach($images as $image):?>
+                    <div class="card col-lg-4" style="width: 18rem;">
+                        <img class="card-img-top" src="<?php echo $image['src']?>" alt="Card image cap" title="<?php echo $image['name']?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $image['name']?></h5>
+                            <p class="card-text"><?php echo $image['description']?></p>
+                            <a href="#" class="btn btn-primary">Go to <?php echo $image['name']?></a>
+                        </div>
+                    </div>
+                <?php endforeach;?>
+            <?php endif;?>
         </div>
     </div>
 </section>
 
-<!--main form-->
+<!--robo-form-->
 
-<section>
-    <div class="robo-form row">
-<!--    <form name="robo-form" id="form" method="get" enctype="multipart/form-data">-->
-<!--        <input type="text" name="login" required>-->
-<!--        <input type="password" name="password" placeholder="Pls enter your password" required>-->
-<!--        <input type="email" name="email" required><br>-->
-<!--        <input type="hidden" name="info"><br>-->
-<!--        <label><input type="checkbox" name="chosen-robot" value="1" checked> Robot 1</label><br>-->
-<!--        <label><input type="checkbox" name="chosen-robot" value="2"> Robot 2</label><br>-->
-<!--        <label><input type="checkbox" name="chosen-robot" value="3"> Robot 3</label><br>-->
-<!--        <label><input type="checkbox" name="chosen-robot" value="4"> Robot 4</label><br>-->
-<!--        <label><input type="checkbox" name="chosen-robot" value="5"> Robot 5</label><br>-->
-<!--        <textarea name="comment"></textarea>-->
-<!--        <input type="submit" value="Send"><br>-->
-<!--    </form>-->
-        <form name="robo-form" id="robo-form" method="get" enctype="multipart/form-data" class="col-lg-8">
-            <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                </div>
+<div class="robo-form row">
+    <form name="robo-form" id="robo-form" method="post" enctype="multipart/form-data" class="col-lg-8">
+        <div class="form-group row">
+            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
             </div>
-            <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword1" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword1" placeholder="Password">
             </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword2" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword2" placeholder="Confirm your password">
+            </div>
+        </div>
 
-<!--checkboxes-->
+    <!--select-->
 
-            <label for="checkboxes-robots"><span class="checkboxes-robots-label">Please vote for the robot do you like</span></label>
-            <div id="checkboxes-robots"class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <label class="checkbox-label">Robot 1
-                        <input type="checkbox" aria-label="Checkbox for following text input"></label>
-                    </div>
-                </div>
-                <input type="text" class="form-control" aria-label="Text input with checkbox">
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Choose the robot do you like</label>
+        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            <option selected>Choose...</option>
+            <option value="robotoy retro">robotoy retro</option>
+            <option value="looksee retro">looksee retro</option>
+            <option value="retro daddy">retro daddy</option>
+            <option value="retro pups">retro pups</option>
+            <option value="retro pirate">retro pirate</option>
+            <option value="cheburashka">cheburashka</option>
+        </select>
+
+    <!--choose file-->
+
+        <div class="form-group uploads">
+            <label for="exampleFormControlFiles">Upload one or more files</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFiles" title="Upload one or more files" multiple>
+        </div>
+
+    <!--Submit button-->
+
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <label class="checkbox-label">Robot 2
-                            <input type="checkbox" aria-label="Checkbox for following text input"></label>
-                    </div>
-                </div>
-                <input type="text" class="form-control" aria-label="Text input with checkbox">
-            </div>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <div class="input-group-text">
-                        <label class="checkbox-label">Robot 3
-                            <input type="checkbox" aria-label="Checkbox for following text input"></label>
-                    </div>
-                </div>
-                <input type="text" class="form-control" aria-label="Text input with checkbox">
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</section>
+        </div>
+    </form>
+</div>
