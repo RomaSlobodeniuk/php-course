4ebd22ce3437f0fa93883c168ff4adc4ea082b56
devsize <artrisefrag@gmail.com>
2018-12-04 20:22:25 +0200

    robots some commit

--- IgorKuznetsov/robots/index.php
+++ IgorKuznetsov/robots/index.php
@@ -18,32 +18,32 @@ if (!empty($params)) {
 $images = [
     [
         'name' => 'robotoy retro',
-        'src' => 'img/00387_robotoy_retro_thumb_alpha.png',
+        'src' => 'assets/img/00387_robotoy_retro_thumb_alpha.png',
         'description' => 'Some robot 1. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ],
     [
         'name' => 'looksee retro',
-        'src' => 'img/00417_looksee_retro_thumb_alpha.png',
+        'src' => 'assets/img/00417_looksee_retro_thumb_alpha.png',
         'description' => 'Some robot 2. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ],
     [
         'name' => 'retro daddy',
-        'src' => 'img/00428_retro_daddy_thumb_alpha.png',
+        'src' => 'assets/img/00428_retro_daddy_thumb_alpha.png',
         'description' => 'Some robot 3. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ],
     [
         'name' => 'retro pups',
-        'src' => 'img/00451_retro_pups_thumb_alpha.png',
+        'src' => 'assets/img/00451_retro_pups_thumb_alpha.png',
         'description' => 'Some robot 4. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ],
     [
         'name' => 'retro pirate',
-        'src' => 'img/00456_retro_pirate_thumb_alpha.png',
+        'src' => 'assets/img/00456_retro_pirate_thumb_alpha.png',
         'description' => 'Some robot 5. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ],
     [
         'name' => 'cheburashka',
-        'src' => 'img/00465_cheburashka_thumb_alpha.png',
+        'src' => 'assets/img/00465_cheburashka_thumb_alpha.png',
         'description' => 'Some robot 6. Some quick example text to build on the card title and make up the bulk of the card\'s content.'
     ]
 ];
