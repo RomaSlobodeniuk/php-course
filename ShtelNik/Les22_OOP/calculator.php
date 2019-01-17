5c158ca782322b8fe6a6e65f4dac1a1f4fe78f77
ShtelNik <shtelnick@gmail.com>
2019-01-17 14:49:44 +0200

    no comment

---
+++ ShtelNik/Les22_OOP/calculator.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nik-2017-12-26
+ * Date: 20.12.2018
+ * Time: 17:55
+ */
---
+++ ShtelNik/Les23_Static/DbConnection.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nik-2017-12-26
+ * Date: 27.12.2018
+ * Time: 20:20
+ */
